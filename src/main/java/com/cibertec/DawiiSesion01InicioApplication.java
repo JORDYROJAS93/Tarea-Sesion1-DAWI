c6862c2988460ed1550be6e96f75d71d6c05d733
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-08-23 21:27:07 -0500

    Inicio

---
+++ src/main/java/com/cibertec/DawiiSesion01InicioApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiSesion01InicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiSesion01InicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/DawiiSesion01InicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiSesion01InicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
